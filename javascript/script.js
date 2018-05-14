758617964d5ec2c2001592dc4a48ed21dfc0137e
Joel Plas <j.g.s.plas@hhs.nl>
2018-05-14 19:52:55 +0200

    Ingeleverd via BB

---
+++ javascript/script.js
@@ -0,0 +1,41 @@
+var timeout;
+
+window.onload = function(){
+    var mijnSlider = new Siema({
+        selector: '.siema',
+        duration: 900,
+        easing: 'ease-out',
+        perPage: 1,
+        startIndex: 0,
+        draggable: true,
+        multipleDrag: true,
+        threshold: 20,
+        loop: true,
+        rtl: false,
+        onInit: () => {},
+        onChange: () => {},
+    });
+    
+    document.getElementsByClassName('links')[0].onclick = function(){
+        mijnSlider.prev(1);
+        clearTimeout(timeout);
+        loop();
+    };
+    
+    document.getElementsByClassName('rechts')[0].onclick = function(){
+        mijnSlider.next(1);
+        clearTimeout(timeout);
+        loop();
+    };
+    
+    function loop(){
+      timeout = setTimeout(function(){
+          mijnSlider.next(1);
+          loop();
+      }, 3000);
+        
+    }
+    
+    loop();
+};
+
